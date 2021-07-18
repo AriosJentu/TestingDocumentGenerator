69c9e369e67c8beedec0be589ccfbd0d41b354a8
AriosJentu <darthpoezd@gmail.com>
2021-07-18 20:54:59 +1000

    Update functions

--- Scripts/Generators/Functions.py
+++ Scripts/Generators/Functions.py
@@ -10,10 +10,11 @@ class Functions:
 	@staticmethod
 	def get_unused_index(from_list: list, cache_list: list) -> int:
 		'''Function to get random index from unused tasks, and this index isn't appeared in cache'''
+		random.seed()
 		randindex = random.randrange(len(from_list))
 
 		#If this element in cache already, then choose another element again
 		if from_list[randindex] in cache_list:
-			return Functions.get_random_index_with_cache(from_list, cache_list)
+			return Functions.get_unused_index(from_list, cache_list)
 		else:
 			return randindex
