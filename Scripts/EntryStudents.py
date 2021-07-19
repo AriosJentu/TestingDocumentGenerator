799e7f80f7984a07e0897a372c803b9412aa34cd
AriosJentu <darthpoezd@gmail.com>
2021-07-20 02:58:41 +1000

    Add module for read students with Entries Reading module

---
+++ Scripts/EntryStudents.py
@@ -0,0 +1,18 @@
+from .Generators import Entries
+from .Generators import Functional
+
+class StudentsReader(Entries.EntriesReader):
+	'''
+	StudentsReader - class with reading information about students in specific format
+	First line of the file is Group Number (or Group Name), all next lines after first are Student Names (list of students)
+	'''
+
+	def generate_information(self) -> Functional.StructList:
+		lines = self.readlines()
+
+		structs = Functional.StructList()
+		for line in lines[1:]:
+			if not Functional.Functions.is_empty_string(line):
+				structs.append(Functional.Struct(student=line, group=lines[0]))
+
+		return structs
