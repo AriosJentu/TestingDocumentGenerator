15af489e84a622696d10532504aec1c55e52fd21
AriosJentu <darthpoezd@gmail.com>
2021-07-20 22:59:00 +1000

    For EntryStudents add more informative comments

--- Scripts/EntryStudents.py
+++ Scripts/EntryStudents.py
@@ -5,9 +5,12 @@ class StudentsReader(Entries.EntriesReader):
 	'''
 	StudentsReader - class with reading information about students in specific format
 	First line of the file is Group Number (or Group Name), all next lines after first are Student Names (list of students)
+	Initial arguments:
+	- 'filepath': Path of the students file
 	'''
 
 	def generate_information(self) -> Functional.StructList:
+		'''Function to generate information. Pattern: fist line is 'group' string, all next lines are 'student' names'''
 		lines = self.readlines()
 
 		structs = Functional.StructList()
