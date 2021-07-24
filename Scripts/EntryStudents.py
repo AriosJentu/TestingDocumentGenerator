48b4731b2c2cbc6c8152ca07fac2e7655dbe82d6
AriosJentu <darthpoezd@gmail.com>
2021-07-25 02:19:53 +1000

    Add class for Empty Entries, without filepath

--- Scripts/EntryStudents.py
+++ Scripts/EntryStudents.py
@@ -1,6 +1,20 @@
 from .Generators import Entries
 from .Generators import Functional
 
+class EmptyStudents(Entries.Entries):
+	'''
+	EmptyStudents - class with generating struct with specific entries count based on class parameter. No need to open files
+	'''
+
+	def generate_information(self) -> Functional.StructList:
+		'''Function to generate information with StudentsReader-like pattern'''
+
+		structs = Functional.StructList()
+		for index in range(self.ENTRIES_COUNT):
+			structs.append(Functional.Struct(student=f"A{index+1}", group="Sample group name"))
+
+		return structs
+
 class StudentsReader(Entries.EntriesReader):
 	'''
 	StudentsReader - class with reading information about students in specific format
@@ -17,21 +31,5 @@ class StudentsReader(Entries.EntriesReader):
 		for line in lines[1:]:
 			if not Functional.Functions.is_empty_string(line):
 				structs.append(Functional.Struct(student=line, group=lines[0]))
-
-		return structs
-
-class EmptyStudents(Entries.EntriesReader):
-	'''
-	EmptyStudents - class with generating struct with specific entries count based on class parameter. No need to open files
-	'''
-
-	ENTRIES_COUNT = 10
-
-	def generate_information(self) -> Functional.StructList:
-		'''Function to generate information with StudentsReader-like pattern'''
-
-		structs = Functional.StructList()
-		for index in range(self.ENTRIES_COUNT):
-			structs.append(Functional.Struct(student=f"A{index+1}", group="Sample group name"))
 
 		return structs
--- Scripts/Generators/Entries.py
+++ Scripts/Generators/Entries.py
@@ -1,13 +1,32 @@
 from . import Functional
 
-class EntriesReader:
+class Entries:
+	'''
+	Entries - parent class for defining custom PageInformation readers by lines. By default it read StructList, which after that will be combine with Variant object, to generate then PageInformation object.
+	This class suppose that there is no file needed. Only one parameter - count of random paths.
+	No initial arguments
+	Parameters:
+	- 'ENTRIES_COUNT': count of the entries to generate
+	'''
+
+	ENTRIES_COUNT = 10
+
+	def __init__(self):
+		pass
+
+	def generate_information(self) -> Functional.StructList:
+		'''Empty function to override. Need to work with entries count information, and generate List of Structs (StructList object). Then this element will be used in generating variant with data presented in this StructList.'''
+		pass
+
+class EntriesReader(Entries):
 	'''
 	EntriesReader - parent class for defining custom PageInformation readers by lines. By default it read StructList, which after that will be combine with Variant object, to generate then PageInformation object.
+	This class suppose to read file
 	Initial arguments:
 	- 'filepath': Path of the file where information will be read
 	'''
 
-	def __init__(self, filepath: str = ""):
+	def __init__(self, filepath: str):
 		self.filepath = filepath
 
 	def read(self):
