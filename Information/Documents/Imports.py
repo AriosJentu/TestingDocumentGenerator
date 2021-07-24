fdbb7633548fad788a8b84f4100b57076d21e1a5
AriosJentu <darthpoezd@gmail.com>
2021-07-24 23:27:02 +1000

    Add document information about Test1

---
+++ Information/Documents/Imports.py
@@ -0,0 +1,7 @@
+import Scripts.Generators.Tasks as Tasks
+import Scripts.Generators.Excercises as Excercises
+import Scripts.Generators.Tests as Tests
+import Scripts.Generators.Documents as Documents
+import Scripts.Generators.Functional as Functional
+import Scripts.EntryStudents as EntryStudents
+import Scripts.Assignments as Assignments
