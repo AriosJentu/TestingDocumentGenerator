ab43af0efebcc7a6b1d21a61984055cd1452fdc8
AriosJentu <darthpoezd@gmail.com>
2021-07-25 01:08:13 +1000

    Move AssignmentsList into Assigments file

--- Scripts/Assignments.py
+++ Scripts/Assignments.py
@@ -65,3 +65,24 @@ class Assignment:
 		else:
 			#Otherwice generate document on path
 			document.generate_document(self.generation_folder+output_file)
+
+class AssignmentsListClass:
+	'''
+	AssignmentsListClass - class which contains information about generated labs.
+	'''
+
+	def __init__(self):
+		self.dict = {}
+
+	def append(self, assignment_class, key: str):
+		'''Function to append assignment class into this module'''
+		if self.dict.get(key) == None:
+			self.dict[key] = {}
+
+		#Append assignment with it's number inside class
+		self.dict[key][assignment_class.number] = assignment_class
+
+	def get(self, key: str, number: int):
+		'''Function to get assignment with it's key and number'''
+		if self.dict.get(key):
+			return self.dict[key].get(number)
