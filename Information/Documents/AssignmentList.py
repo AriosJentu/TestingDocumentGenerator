692f7dccbf879f7a3485d4ac1b81e7192bdeaef6
AriosJentu <darthpoezd@gmail.com>
2021-07-24 23:27:18 +1000

    Add AssignmentList object to contain information about tasks

---
+++ Information/Documents/AssignmentList.py
@@ -0,0 +1,28 @@
+class AssignmentsListClass:
+	'''
+	AssignmentsListClass - class which contains information about generated labs.
+	'''
+
+	def __init__(self):
+		self.dict = {}
+
+	def append(self, assignment_class, key: str):
+		'''Function to append assignment class into this module'''
+		if self.dict.get(key) == None:
+			self.dict[key] = {}
+
+		#Append assignment with it's number inside class
+		self.dict[key][assignment_class.number] = assignment_class
+
+	def get(self, key: str, number: int):
+		'''Function to get assignment with it's key and number'''
+		if self.dict.get(key):
+			return self.dict.get(number)
+
+AssignmentsList = AssignmentsListClass()
+
+#Imports 
+from . import Test1
+
+#Append assignment Test1 into Assignments List
+AssignmentsList.append(Test1.Test1, "t")
