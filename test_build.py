18dfd59a706058418cd61de9190b909828b9ac39
AriosJentu <darthpoezd@gmail.com>
2021-07-28 03:01:59 +1000

    Update test build with new AssignmentList and updated AssignmentInformation

--- test_build.py
+++ test_build.py
@@ -1,12 +1,23 @@
-import Information.Documents.AssignmentsList as AssignmentsList
+import Information.Documents.AssignmentsInformation as AssignmentsInformation
 import Scripts.EntryStudents as EntryStudents
+import Scripts.Assignments as Assignments
 
 students_location = "Information/Students/students.txt"
 students_entries = EntryStudents.EmptyStudents()
-students_entries.ENTRIES_COUNT = 20
+students_entries.ENTRIES_COUNT = 5
 
-Test1 = AssignmentsList.AssignmentsList.get("t", 1)
+Test1 = AssignmentsInformation.AssignmentsInformation.get("t", 1)
+Test2 = AssignmentsInformation.AssignmentsInformation.get("t", 2)
 
-work = Test1()
-work.set_entries(students_entries)
-work.generate("generated_new.tex")
+work1 = Test1()
+work1.set_entries(students_entries)
+
+work2 = Test2()
+work2.set_entries(students_entries)
+
+assignments = Assignments.AssignmentsList()
+assignments.append(work1)
+assignments.append(work2)
+
+assignments.generate("document.tex")
+	
