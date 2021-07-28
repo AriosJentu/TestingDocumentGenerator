5ee0f2da1484f79f8ac0751daaa6a6e2e8b85747
AriosJentu <darthpoezd@gmail.com>
2021-07-28 17:02:52 +1000

    Update arguments, add function to sed default entries classes, rename parse_key function, update parse_entries with checkers instances

--- Scripts/Parser/Arguments.py
+++ Scripts/Parser/Arguments.py
@@ -1,4 +1,3 @@
-import os.path
 from ..Generators import Functional
 from ..Generators import Entries
 from . import Parser
@@ -8,51 +7,57 @@ class ArgumentsParser:
 	ArgumentsParser - class to parse arguments to generate tasks
 	Initial arguments:
 	- 'keys': string in assignment-string values format (like 'l1-4,7' etc, presented in Parser module)
-	- 'entries': string/int/dictionary argument. If string, it may be a filepath of the entries, or one of the entry name (for example, student, if filepath not found). If int, it may be count of generated specifically entries, with case 'no file'. 
-	- 'group_string': string (may be empty) with group name, in case of specific student generator.
+	- 'entries': string/int/dictionary/Entries argument. If string, it may be a filepath of the entries, or one of the entry name (for example, student, if filepath not found). If int, it may be count of generated specifically entries, with case 'no file'. 
 	'''
 
-	def __init__(self, keys: str, entries: [str, int, dict, Functional.StructList], *args, **kwargs):
+	def __init__(self, keys: str, entries: (str, int, dict, Entries.Entries), *args, **kwargs):
 
-		if not isinstance(entries, (str, int, dict, Functional.StructList)):
-			entries = {}
+		#If entries not intance of presented classes, then just make it empty, then it will generate entries without path
+		if not isinstance(entries, (str, int, dict, Entries.Entries)):
+			entries = None
 
 		self.keys = keys
 		self.entries = entries
-		self.group_string = group_string
 
-	def parse_keys(self) -> (str, list[int]):
+		#Set classes as parameters of the class
+		self.entries_with_path = Entries.EntriesReader
+		self.entries_no_path = Entries.Entries
+		self.entries_from_values = Entries.EntryFromValues
+
+	def set_entries_class(self, with_path = Entries.EntriesReader, no_path = Entries.Entries, from_values = Entries.EntryFromValues):
+		'''Function to update entries class - with_path, no_path and from_values'''
+		self.entries_with_path = with_path
+		self.entries_no_path = no_path
+		self.entries_from_values = from_values
+
+	def parse_assignments_keynumbers(self) -> list[Parser.KeyNumbers]:
 		'''Function to parse keys'''
-		return Parser.parse_assignments_argument(self.keys)
+		return Parser.Parser.parse_assignments_argument(self.keys)
 
-	def parse_entries(self, entries_with_path = Entries.EntriesReader, entries_no_path = Entries.Entries) -> Functional.StructList:
-		'''
-		Function to parse entries.
-		Arguments:
-		- 'entries_with_path': class of generating entries with file reading options
-		- 'entries_no_path': class of generating entries without reading, generate by count
-		'''
+	def parse_entries(self) -> Entries.Entries:
+		'''Function to parse entries'''
 
 		#If this path exists, return entries with reading file
-		if os.path.isfile(self.entries):
-			entries = entries_with_path(self.entries)
+		if isinstance(self.entries, str) and Functional.Path.isfile(self.entries):
+			entries = self.entries_with_path(self.entries)
 			return entries
 
 		#If type of entries is dictionary, then generate struct list from this dictionary
-		if isinstance(entries, dict):
-			return Functional.StructList([Functional.Struct(**self.entries)])
+		if isinstance(self.entries, dict):
+			entries = self.entries_from_values(self.entries)
+			return entries
 
 		#If this is integer, return entries without filepath with count of entries
-		if type(self.entries) == int or self.entries.isdecimal():
-			entries = entries_no_path()
+		if isinstance(self.entries, int) or (isinstance(self.entries, str) and self.entries.isdecimal()):
+			entries = self.entries_no_path()
 			entries.ENTRIES_COUNT = int(self.entries)
 			return entries
 			
-		#If type of entries is struct list, then return itself
-		if isinstance(entries, Functional.StructList):
-			return entries
+		#If type of entries is class Entries, then return itself
+		if isinstance(self.entries, Entries.Entries):
+			return self.entries
 
 		#If there is no pattern for this element, just return empty struct:
-		return entries_no_path()
+		return self.entries_no_path()
 
 
