9bf734b058bcaa5ca54d2c8f5a9d520b9ed02189
AriosJentu <darthpoezd@gmail.com>
2021-08-03 23:04:59 +1000

    Move imports element into Modules folder

---
+++ Modules/Imports.py
@@ -0,0 +1,7 @@
+import Scripts.Generators.Tasks as Tasks
+import Scripts.Generators.Excercises as Excercises
+import Scripts.Generators.Tests as Tests
+import Scripts.Generators.Documents as Documents
+import Scripts.Functions as Functions
+import Scripts.EntryStudents as EntryStudents
+import Scripts.Assignments as Assignments
