8c2a84718f10e2602f3015c977940aacf2b81aeb
AriosJentu <darthpoezd@gmail.com>
2021-08-09 15:34:11 +1000

    Add variable 'MODULE_PATH' for Imports

--- Modules/Imports.py
+++ Modules/Imports.py
@@ -2,6 +2,8 @@ import Scripts.Generators.Tasks as Tasks
 import Scripts.Generators.Excercises as Excercises
 import Scripts.Generators.Tests as Tests
 import Scripts.Generators.Documents as Documents
+import Scripts.Parser.Modules as Modules
 import Scripts.Functions as Functions
 import Scripts.EntryStudents as EntryStudents
 import Scripts.Assignments as Assignments
+MODULE_PATH = Modules.CurrentModule.get_current_module_path()
